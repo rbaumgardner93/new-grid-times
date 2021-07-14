2f6ae3ffbeec60d80209c9b7883c70dfe90897c2
Ryan Baumgardner <rbaumgardner@planview.com>
2021-07-14 10:00:10 -0500

    complete exercise 1

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -29,7 +29,19 @@ const Header = () => {
         </Row>
       </SuperHeader>
       <MainHeader>
+		  <ActionGroup>
+			<button>
+			  <Search size={24} />
+			</button>
+			<button>
+			  <Menu size={24} />
+			</button>
+		  </ActionGroup>
         <Logo />
+	  	<ButtonGroup>
+	  		<Button>Subscribe</Button>
+	  		<a href="/">Already a subscriber?</a>
+	  	</ButtonGroup>
       </MainHeader>
     </header>
   );
@@ -39,6 +51,10 @@ const SuperHeader = styled.div`
   padding: 16px 0;
   background: var(--color-gray-900);
   color: white;
+
+  @media( ${QUERIES.desktopAndUp} ) {
+	display: none;
+  }
 `;
 
 const Row = styled(MaxWidthWrapper)`
@@ -59,12 +75,51 @@ const ActionGroup = styled.div`
   }
 `;
 
+const ButtonGroup = styled.div`
+	display: none;
+
+  a {
+	font-size: ${ 14 / 16};
+	text-decoration: underline;
+	font-style: italic;
+	color: var(--color-gray-900);
+  }
+
+  @media( ${QUERIES.desktopAndUp} ) {
+	  display: flex;
+	  flex-direction: column;
+	  align-items: center;
+	  gap: 8px;
+	  align-self: flex-end;
+  }
+
+`;
+
 const MainHeader = styled(MaxWidthWrapper)`
   display: flex;
   align-items: center;
   justify-content: center;
   margin-top: 32px;
   margin-bottom: 48px;
+
+  & > ${ActionGroup} {
+	display: none;
+  }
+
+  @media( ${QUERIES.tabletAndUp} ) {
+	  margin-top: 48px;
+	  margin-bottom: 84px;
+  }
+
+  @media( ${QUERIES.desktopAndUp} ) {
+	  justify-content: space-between;
+	  margin-top: 16px;
+	  margin-bottom: 72px;
+
+	  & > ${ActionGroup} {
+		display: flex;
+	  }
+  }
 `;
 
 export default Header;
