68d7e110ed9f8e74e8e820a14eac54fc6944effa
Ryan Baumgardner <rbaumgardner@planview.com>
2021-07-14 14:42:51 -0500

    complete exercise 4

--- src/components/SpecialtyStoryGrid/SpecialtyStoryGrid.js
+++ src/components/SpecialtyStoryGrid/SpecialtyStoryGrid.js
@@ -3,6 +3,8 @@ import styled from 'styled-components/macro';
 
 import { MARKET_DATA, SPORTS_STORIES } from '../../data';
 
+import { QUERIES } from "../../constants.js";
+
 import MarketCard from '../MarketCard';
 import SectionTitle from '../SectionTitle';
 import MiniStory from '../MiniStory';
@@ -36,7 +38,9 @@ const SpecialtyStoryGrid = () => {
         </SectionTitle>
         <SportsStories>
           {SPORTS_STORIES.map((data) => (
-            <MiniStory key={data.id} {...data} />
+			<SportStoryContainer key={data.id}>
+            	<MiniStory {...data} />
+			</SportStoryContainer>
           ))}
         </SportsStories>
       </SportsSection>
@@ -47,14 +51,48 @@ const SpecialtyStoryGrid = () => {
 const Wrapper = styled.div`
   display: grid;
   gap: 48px;
+
+  @media(${QUERIES.tabletAndUp}) {
+	gap: 64px;
+  	grid-template-columns: minmax(0, auto);
+  }
+
+  @media(${QUERIES.desktopAndUp}) {
+	gap: 0;
+  	grid-template-columns: 1fr minmax(0px, 1fr);
+  }
 `;
 
-const MarketsSection = styled.section``;
+const MarketsSection = styled.section`
+	@media(${QUERIES.desktopAndUp}) {
+		border-right: 1px solid var(--color-gray-300);
+		margin-right: 16px;
+		padding-right: 16px;
+	}
+`;
 
-const MarketCards = styled.div``;
+const MarketCards = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(175px, 1fr));
+  gap: 16px;
+`;
 
 const SportsSection = styled.section``;
 
-const SportsStories = styled.div``;
+const SportStoryContainer = styled.div`
+  min-width: 220px;
+`;
+
+const SportsStories = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(175px, 1fr));
+  gap: 16px;
+
+  @media(${QUERIES.tabletAndUp}) {
+	display: flex;
+	grid-template-columns: revert;
+	overflow: auto;
+  }
+`;
 
 export default SpecialtyStoryGrid;
